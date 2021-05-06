22b7af47cab5bf1cc3ab37dc6db0028be0740a1e
LAPTOP-JAEFD5TU\lucas <lucaswizard98@gmail.com>
2021-05-06 13:01:43 +0200

    +test bookmark

---
+++ src/main/java/at/fhcampus/domain/Bookmark.java
@@ -0,0 +1,4 @@
+package at.fhcampus.domain;
+
+public class Bookmark {
+}
---
+++ src/main/test/java/at/fhcampus/domain/BookmarkTest.java
@@ -0,0 +1,7 @@
+package at.fhcampus.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookmarkTest {
+
+}
