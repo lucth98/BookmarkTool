3ae63bf95f592d2cdab7f97aa142a0e148dc3040
LAPTOP-JAEFD5TU\lucas <lucaswizard98@gmail.com>
2021-05-07 14:54:49 +0200

    + main methods

--- src/main/java/at/fhcampus/domain/Bookmark.java
+++ src/main/java/at/fhcampus/domain/Bookmark.java
@@ -1,4 +1,47 @@
 package at.fhcampus.domain;
 
+import java.util.ArrayList;
+
 public class Bookmark {
+
+    private String url;
+
+    private ArrayList<String> tags=new ArrayList<String>();
+
+    private int rating;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public ArrayList<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(ArrayList<String> tags) {
+        this.tags = tags;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public Bookmark(String url) {
+        this.url = url;
+    }
+
+    public Bookmark(String url, String tag) {
+        this.url = url;
+        this.tags.add(tag);
+
+
+    }
 }
--- src/main/java/at/fhcampus/domain/BookmarkManager.java
+++ src/main/java/at/fhcampus/domain/BookmarkManager.java
@@ -1,4 +1,47 @@
 package at.fhcampus.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookmarkManager {
+
+ private ArrayList<Bookmark> bookmarkArrayList=new ArrayList<>();
+
+    public ArrayList<Bookmark> getBookmarkArrayList() {
+        return bookmarkArrayList;
+    }
+
+    public void setBookmarkArrayList(ArrayList<Bookmark> bookmarkArrayList) {
+        this.bookmarkArrayList = bookmarkArrayList;
+    }
+
+    public void addBookmark(String url){
+
+    }
+
+    public void addTagToBookmark(){
+
+    }
+
+    public int getNumbersOfSecureURL(){
+        return 0;
+    }
+
+    static public boolean validateURL(String url){
+        return  false;
+    }
+
+    public List<Bookmark> filterByTags(List<String> tags){
+
+        return null;
+    }
+
+
+
+
+
+
+
+
+
 }
