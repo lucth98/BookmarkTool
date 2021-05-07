2bc05c93153bc8b686b8fdce0ccc09d33cde5fdc
LAPTOP-JAEFD5TU\lucas <lucaswizard98@gmail.com>
2021-05-07 14:37:00 +0200

    + bookmark manager

--- src/main/test/java/at/fhcampus/domain/BookmarkTest.java
+++ src/main/test/java/at/fhcampus/domain/BookmarkTest.java
@@ -1,7 +1,17 @@
 package at.fhcampus.domain;
 
+import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BookmarkTest {
+
+
+
+
+    @Test
+    public void test(){
+
+    }
 
 }
