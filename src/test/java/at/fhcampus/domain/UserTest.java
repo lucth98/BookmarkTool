f9d638755d34c238d68eaeac59acff3f1860f5b1
LAPTOP-JAEFD5TU\lucas <lucaswizard98@gmail.com>
2021-06-08 15:37:39 +0200

    +ensureThatBookmarkMangerCanNotBeSaved1

--- src/test/java/at/fhcampus/domain/UserTest.java
+++ src/test/java/at/fhcampus/domain/UserTest.java
@@ -2,6 +2,7 @@ package at.fhcampus.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.Locale;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class UserTest {
 
     @Test
-    public void ensureThatBookmarkmangerCanBeSaved() {
+    public void ensureThatBookmarkMangerCanBeSaved() {
         // Arrange
         User user =new User();
 
@@ -37,8 +38,39 @@ class UserTest {
         }
     }
 
+
     @Test
-    public void ensureThatBookmarkmangerCanBeLoaded() {
+    public void ensureThatBookmarkMangerCanNotBeSaved1() {
+        // Arrange
+        User user =new User();
+
+        BookmarkManager bookmarkManager =user.getBookmarkManager();
+
+        String url1 = "http://test.com/Test";
+        String url2 = "http://tester.com/Test";
+        String tag1 = "test";
+        String tag2 = "tester";
+
+        Bookmark bookmark1 =new Bookmark(url1,tag1);
+        Bookmark bookmark2 =new Bookmark(url2,tag2);
+
+        bookmarkManager.getBookmarkArrayList().add(bookmark1);
+        bookmarkManager.getBookmarkArrayList().add(bookmark2);
+
+        String fileName=null;
+        // Act &  Assert
+        NullPointerException nullPointerException=assertThrows(NullPointerException.class,()-> {user.saveBookmarkManager(fileName);});
+       // IOException ioException =assertThrows(IOException.class,()-> {user.saveBookmarkManager(fileName);});
+//        try {
+//            user.saveBookmarkManager(fileName);
+//        }catch (Exception e){
+//            System.out.println(e);
+//            fail();
+//        }
+    }
+
+    @Test
+    public void ensureThatBookmarkMangerCanBeLoaded() {
         // Arrange
         User user =new User();
 
