7c2e89cbc60aa1daad50a7f77101d6324bed9a05
LAPTOP-JAEFD5TU\lucas <lucaswizard98@gmail.com>
2021-06-10 16:59:24 +0200

    test if scanner cann be set

--- src/main/java/at/fhcampus/domain/Main.java
+++ src/main/java/at/fhcampus/domain/Main.java
@@ -1,8 +1,16 @@
 package at.fhcampus.domain;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+    }
+
+    //test if scanner cann be set
+    public String read(){
+        Scanner scanner =new Scanner(System.in);
+        return scanner.nextLine();
     }
 }
--- src/test/java/at/fhcampus/domain/MainTest.java
+++ src/test/java/at/fhcampus/domain/MainTest.java
@@ -1,7 +1,55 @@
 package at.fhcampus.domain;
 
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
 class MainTest {
+
+
+        //test if scanner cann be set
+    @Test
+    public void     testIfScannerCannBeSet() {
+        // Arrange
+
+        Main main=new Main();
+
+        String expectedResult="test";
+        String actualResult="";
+        String data = expectedResult+"\r\n";
+        InputStream inputStream = System.in;
+        try {
+            System.setIn(new ByteArrayInputStream(data.getBytes()));
+
+            // Act
+             actualResult =main.read();
+
+            //Assert
+
+            assertEquals(expectedResult,actualResult);
+
+        } catch (Exception e){
+            System.setIn(inputStream);
+            System.out.println(e);
+        }
+        finally {
+            System.setIn(inputStream);
+            System.out.println(actualResult);
+        }
+
+
+    }
+
+
 
 }
